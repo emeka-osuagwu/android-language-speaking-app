1cf31b39f5a311629a2905409cfcb6b099f43966
emeka osuagwu <emekaosuagwu@emekas-MacBook-Pro.local>
2019-01-26 13:23:46 +0100

    add word context to app

--- app/src/main/java/com/carbontech/alejo/myapplication/MainActivity.java
+++ app/src/main/java/com/carbontech/alejo/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.carbontech.alejo.myapplication;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,4 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void speak(View view) {
+
+        int button_id = view.getId();
+
+        String tapped_button =  view.getResources().getResourceEntryName(button_id);
+
+        int resource_id = getResources().getIdentifier(tapped_button, "raw", "com.carbontech.alejo.myapplication");
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resource_id);
+
+        mediaPlayer.start();
+    }
 }
+
